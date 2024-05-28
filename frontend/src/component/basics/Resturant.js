96a3e165f7e8ca52544af88515afbe3cf6efc076
leonischu <leonischu10@gmail.com>
2024-05-28 22:03:37 +0545

    Resturant project

---
+++ frontend/src/component/basics/Resturant.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Style.css";
+
+const Resturant = () => {
+  return (
+    <>
+      
+<div className="card-container">
+    <div className="card">
+        <div className="card-body">
+            <span className='card-number card-circle subtle'>1</span>
+        </div>
+    </div>
+</div>
+    </>
+  )
+}
+
+export default Resturant

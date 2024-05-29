c62bee260501b00d1afac8dd9c0082bad489cee8
leonischu <leonischu10@gmail.com>
2024-05-29 20:09:46 +0545

    Resturant

---
+++ frontend/src/component/basics/MenuCard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const MenuCard = ({menuData}) => {
+  return (
+    <>
+    <section className="main-card--cointainer">
+    {
+        menuData.map((curEle)=>{
+            return(<>   
+                <div className="card-container" key={curEle.id}>
+                    <div className="card">
+                        <div className="card-body">
+                            <span className='card-number card-circle subtle'>{curEle.id}</span>
+                            <span className="card-author subtle">{curEle.name}</span>
+                            <h2 className="card-title">{curEle.name}</h2>
+                            <span className="card-description subtle">{curEle.description}</span>
+                             <div className="card-read">Read</div>
+                        </div>
+                        <img src={curEle.image} alt="image" className='card-media' />
+                        <span className="card-tag subtle">Order Now</span>
+                    </div>
+                </div></>
+            
+            
+            )
+
+        })
+    }
+    </section>
+   
+        </>
+  )
+}
+
+export default MenuCard
--- frontend/src/component/basics/Resturant.js
+++ frontend/src/component/basics/Resturant.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Style.css";
+import Menu from './menuApi';
+import MenuCard from './MenuCard';
+const uniqueList = Menu.map((curEle)=>{
+  return curEle.category;
 
+
+
+})
 const Resturant = () => {
+  const[menuData,setMenuData]=useState(Menu);
+  const filterItem= (category) =>{
+    const updatedList = Menu.filter((curEle)=>{
+      return curEle.category===category;
+    });
+setMenuData(updatedList);
+  };
   return (
-    <>
-      
-<div className="card-container">
-    <div className="card">
-        <div className="card-body">
-            <span className='card-number card-circle subtle'>1</span>
-        </div>
+   <>
+   <nav className="navbar">
+    <div className="btn-group">
+      <button className="btn-group__item" onClick={()=>filterItem("breakfast")}>Breakfast</button>
+      <button className="btn-group__item" onClick={()=>filterItem("lunch")}>Lunch</button>
+      <button className="btn-group__item"onClick={()=>filterItem("evening")}>Evening</button>
+      <button className="btn-group__item"onClick={()=>filterItem("dinner")}>Dinner</button>
+      <button className="btn-group__item"onClick={()=>setMenuData(Menu)}>All</button>
+
     </div>
-</div>
-    </>
+   </nav>
+   <MenuCard menuData={menuData}/>
+   </>
   )
 }
 
